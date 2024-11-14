c4ff47687e53875adcddbd7ee0058ae4f0f7884b
Saif23489 <118176074+Saif23489@users.noreply.github.com>
2024-11-14 08:25:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,26 +7,26 @@
 #include "stack.h"
 
 int main() {
-    // Load map from file
+    
     t_map map = createMapFromFile("C:\\Users\\malom\\CLionProjects\\int2_group10\\maps\\example1.map");
     printf("Map created with dimensions %d x %d\n", map.y_max, map.x_max);
 
-    // Display the map's soil grid
+   
     printf("Map Soils:\n");
     displayMap(map);
 
-    // Initialize robot localisation
+   
     t_localisation loc = loc_init(5, 5, NORTH);
     printf("\nInitial Robot Position: (%d, %d), Orientation: %d\n", loc.pos.x, loc.pos.y, loc.ori);
 
-    // Perform a series of moves
+  
     updateLocalisation(&loc, F_10);
     printf("After Move F_10: (%d, %d), Orientation: %d\n", loc.pos.x, loc.pos.y, loc.ori);
 
     updateLocalisation(&loc, T_RIGHT);
     printf("After Turn Right: (%d, %d), Orientation: %d\n", loc.pos.x, loc.pos.y, loc.ori);
 
-    // Queue example for path traversal
+  
     t_queue queue = createQueue(10);
     t_position start_pos = {2, 3};
 
@@ -40,7 +40,7 @@ int main() {
         printf("Queue is empty; cannot dequeue.\n");
     }
 
-    // Cleanup allocated memory for the queue
+   
     free(queue.values);
 
     return 0;
