96af37bb451038ad84d3195d7e7975b5748e0d67
sffdz <117524756+sffdz@users.noreply.github.com>
2024-11-25 00:35:36 +0100

    Update stack.c

---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/loc.c.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/untitled1.dir/loc.c.obj: \
+ C:\Users\malom\CLionProjects\untitled39\new\loc.c \
+ C:\Users\malom\CLionProjects\untitled39\new\loc.h
---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/moves.c.obj.d
@@ -0,0 +1,4 @@
+CMakeFiles/untitled1.dir/moves.c.obj: \
+ C:\Users\malom\CLionProjects\untitled39\new\moves.c \
+ C:\Users\malom\CLionProjects\untitled39\new\moves.h \
+ C:\Users\malom\CLionProjects\untitled39\new\loc.h
